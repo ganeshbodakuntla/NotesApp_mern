9ebb4a8bee3e055d30a5a20f1644ea744858c5e5
ganeshbodakuntla <ganeshbodakuntla@gmail.com>
2025-05-22 15:55:35 +0530

    prepared for deployement

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,20 +4,33 @@ import dotenv from "dotenv";
 import noteRoutes from "./Routes/noteRoutes.js";
 import { connectdB } from "./config/dB.js";
 import rateLimiter from "./middleWares/rateLimiter.js";
+import path from "path";
 
 dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 3001;
+const _dirname = path.resolve();
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173", // only allow this origin
+    })
+  );
+}
+
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:5173", // only allow this origin
-  })
-);
 app.use(rateLimiter);
 
 app.use("/api/notes", noteRoutes);
-const PORT = process.env.PORTv || 3001;
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(_dirname, "../Frontend/dist")));
+
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(_dirname, "../Frontend", "dist", "index.html"));
+  });
+}
 connectdB().then(() => {
   app.listen(PORT, () => {
     console.log(`server running on port:${PORT}`);
